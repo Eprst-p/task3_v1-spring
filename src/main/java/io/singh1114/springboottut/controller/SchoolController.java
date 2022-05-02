313ba490e3c575b4e04b33c17d8b03beda3d4c6f
Eprst <easter_p@mail.ru>
2022-05-02 20:21:20 +0300

    correction of api-methods

--- src/main/java/io/singh1114/springboottut/controller/SchoolController.java
+++ src/main/java/io/singh1114/springboottut/controller/SchoolController.java
@@ -2,34 +2,40 @@ package io.singh1114.springboottut.controller;
 
 import io.singh1114.springboottut.school.School;
 import io.singh1114.springboottut.school.SchoolService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/schools")
 public class SchoolController {
 
     @Autowired
     private SchoolService schoolService;
 
-    @RequestMapping("/schools")
+    @GetMapping
+    @ApiOperation("Получить все школы")
     public List<School> getAllSchools() {
         return schoolService.getAllSchools();
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/school")
+    @PostMapping
+    @ApiOperation("Добавить школу")
     public void addSchool(@RequestBody School school) {
         schoolService.addSchool(school);
     }
 
-    @DeleteMapping("/school/{schoolId}")
+    @DeleteMapping("/{schoolId}")
+    @ApiOperation("Удалить школу")
     public void removeSchool(@PathVariable String schoolId) {
         schoolService.removeSchool(Long.parseLong(schoolId));
     }
 
-    @PutMapping("/school/{schoolId}")
+    @PutMapping("/{schoolId}")
+    @ApiOperation("Пофиксить школу")
     public void updateSchool(@PathVariable String schoolId, @RequestBody School school) {
         schoolService.updateSchool(school, Long.parseLong(schoolId));
     }
